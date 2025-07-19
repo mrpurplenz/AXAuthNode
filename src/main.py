576b60579abb5221a1e3fa07f6e9fca0cf9395e7
Richard Edmonds <mrpurplenz@gmail.com>
2025-07-19 21:14:33 +1200

    Initial code structure for AXAuthNode

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+def main():
+    print("AXAuthNode server starting...")
+    # TODO: Initialize modules, radio interface, and auth here
+
+if __name__ == "__main__":
+    main()
